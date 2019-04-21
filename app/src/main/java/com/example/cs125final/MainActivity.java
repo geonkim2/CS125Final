d5ba0e63818baea7191f3e74fc9eb03d42de6d28
emiliakedainis <46225941+emiliakedainis@users.noreply.github.com>
2019-04-20 21:37:44 -0500

    made background work, ideally made the title format correct, trying to make my android studio actually run apps ;-;

--- app/src/main/java/com/example/cs125final/MainActivity.java
+++ app/src/main/java/com/example/cs125final/MainActivity.java
@@ -3,11 +3,24 @@ package com.example.cs125final;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//This is everything they imported on the json mp
+//So I just copied and pasted lmao
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView titleText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        titleText = findViewById(R.id.title);
+        titleText.bringToFront();
     }
 }
