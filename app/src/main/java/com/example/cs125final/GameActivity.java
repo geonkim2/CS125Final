559cd67c0f9c69319a8263536da0ee7289bbe017
emiliakedainis <46225941+emiliakedainis@users.noreply.github.com>
2019-04-21 23:17:25 -0500

    trying to make buttons work while invisible, trying to make app switch screens after a specified number of seconds

--- app/src/main/java/com/example/cs125final/GameActivity.java
+++ app/src/main/java/com/example/cs125final/GameActivity.java
@@ -1,7 +1,10 @@
 package com.example.cs125final;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 /**
  * MediaPlayer ring= MediaPlayer.create(MainActivity.this,R.raw.ring);
@@ -9,10 +12,29 @@ import android.os.Bundle;
  */
 public class GameActivity extends AppCompatActivity {
 
+    Button down;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_game);
+
+        /** this is here for DEBUGGING PURPOSES*/
+        down = findViewById(R.id.down_button);
+        down.setEnabled(true);
+        down.setOnClickListener((v) -> nextScreen());
+
+        // im trying to make it change screens after a small delay
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(GameActivity.this, ResultActivity.class);
+                GameActivity.this.startActivity(intent);
+                GameActivity.this.finish();
+            }
+        }, 400);
+    }
+    public void nextScreen() {
+        setContentView(R.layout.activity_result);
     }
 }
-
