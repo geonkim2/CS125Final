0ee86f8de02a47b76e2952a1d814f47593b3d4d9
emiliakedainis <46225941+emiliakedainis@users.noreply.github.com>
2019-04-23 13:22:25 -0500

    no differences

--- app/src/main/java/com/example/cs125final/GameActivity.java
+++ app/src/main/java/com/example/cs125final/GameActivity.java
@@ -5,8 +5,7 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
-
-import javax.xml.transform.Result;
+import android.widget.ImageView;
 
 /**
  * MediaPlayer ring= MediaPlayer.create(MainActivity.this,R.raw.ring);
@@ -17,14 +16,44 @@ public class GameActivity extends AppCompatActivity {
     Button down;
     Button quit;
 
+    ImageView leftArrow;
+    ImageView upArrow;
+    ImageView downArrow;
+    ImageView rightArrow;
+    Button left;
+    Button up;
+    Button right;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        leftArrow = findViewById(R.id.left_image);
+        leftArrow.bringToFront();
+        left = findViewById(R.id.left_button);
+        left.setEnabled(true);
+        left.bringToFront();
+
+        upArrow = findViewById(R.id.up_image);
+        upArrow.bringToFront();
+        up = findViewById(R.id.up_button);
+        up.setEnabled(true);
+        up.bringToFront();
+
+        rightArrow = findViewById(R.id.right_image);
+        rightArrow.bringToFront();
+        right = findViewById(R.id.right_button);
+        right.setEnabled(true);
+        right.bringToFront();
+
+        downArrow = findViewById(R.id.down_image);
+        downArrow.bringToFront();
+
         /** this is here for DEBUGGING PURPOSES*/
         down = findViewById(R.id.down_button);
         down.setEnabled(true);
+        down.bringToFront();
         down.setOnClickListener((v) -> nextScreen());
 
         quit = findViewById(R.id.quit);
@@ -41,7 +70,7 @@ public class GameActivity extends AppCompatActivity {
                 GameActivity.this.startActivity(intent);
                 GameActivity.this.finish();
             }
-            // add three zeros to however seconds you want to make it
+            // add three zeros to however seconds x you want to make it
         }, 4000);
     }
     public void nextScreen() {
